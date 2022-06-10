b64f9615c706161dff082faf6acd47f3b053d4ca
Jefferson Jadoc <70456980+ashwallbera@users.noreply.github.com>
2022-06-10 17:30:57 +0800

    [ADD] User controller for firebase databases

---
+++ JEAspNetCore/Controllers/UserController.cs
@@ -0,0 +1,79 @@
+﻿
+
+using Firebase.Database;
+using Firebase.Database.Query;
+using JEAspNetCore.Model;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace JEAspNetCore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        string AuthSecret = "A4mpOGwFLos77X7uJMttMaeBPqURI1vKdQ2iwWBK";
+        string BasePath = "https://jadocenterprises-default-rtdb.asia-southeast1.firebasedatabase.app";
+        private readonly FirebaseClient firebaseClient;
+
+        public UserController()
+        {
+            firebaseClient = new FirebaseClient(
+              BasePath,
+              new FirebaseOptions
+              {
+                  AuthTokenAsyncFactory = () => Task.FromResult(AuthSecret)
+              });
+        }
+        // GET: api/<UserController>
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<UserModel>>> Get()
+        {
+            var result = await firebaseClient
+                    .Child("users")
+                    .OnceAsync<UserModel>();
+            return Ok(result);
+        }
+
+        // GET api/<UserController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<UserController>
+        [HttpPost]
+        public async Task<ActionResult<IEnumerable<UserModel>>> Post(UserModel value)
+        {
+            try
+            {
+
+                var result = await firebaseClient
+                    .Child("users")
+                    .PostAsync(value);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+
+               return Ok(ex.StackTrace);
+            }
+
+            
+        }
+
+        // PUT api/<UserController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<UserController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ JEAspNetCore/Model/UserModel.cs
@@ -0,0 +1,33 @@
+﻿using System.Runtime.Serialization;
+
+namespace JEAspNetCore.Model
+{
+    
+    [DataContract]
+    public class UserModel
+    {
+        [DataMember]
+        public string? id { get; set; }
+        [DataMember]
+        public string? username { get; set; }
+        [DataMember]
+        public string? password { get; set; }
+        [DataMember]
+        public string? fname { get; set; }
+        [DataMember]
+        public string? lname { get; set; }
+        [DataMember]
+        public string? mname { get; set; }
+
+        [DataMember]
+        public string? address { get; set; }
+        [DataMember]
+        public string? contactno { get; set; }
+        [DataMember]
+        public string? role { get; set; }
+
+        [DataMember]
+        public string isDeleted { get; set; }
+
+    }
+}
