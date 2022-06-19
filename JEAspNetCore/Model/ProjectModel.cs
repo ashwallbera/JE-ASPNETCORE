9aa375373845f5ac6bd58b493e1d34a719cd127c
Jefferson Jadoc <70456980+ashwallbera@users.noreply.github.com>
2022-06-19 16:33:20 +0800

    [ADD] Project Model

---
+++ JEAspNetCore/Model/ProjectModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.Serialization;
+
+namespace JEAspNetCore.Model
+{
+    [DataContract]
+    public class ProjectModel
+    {
+        [DataMember]
+        public string? id { get; set; }
+
+        [DataMember]
+        public string? name { get; set; }
+        [DataMember]
+        public string? location { get; set; }
+        [DataMember]
+        public string? isFinished { get; set; }
+    }
+}
