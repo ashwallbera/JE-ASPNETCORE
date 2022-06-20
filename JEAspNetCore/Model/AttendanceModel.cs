30b4cd9b7cb467cf54d6a74fdcdd6a50f1cc3375
Jefferson Jadoc <70456980+ashwallbera@users.noreply.github.com>
2022-06-20 21:43:02 +0800

    [ADD] Create Model for attendance

---
+++ JEAspNetCore/Model/AttendanceModel.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.Serialization;
+
+namespace JEAspNetCore.Model
+{
+    [DataContract]
+    public class AttendanceModel
+    {
+        [DataMember]
+        public string id { get; set; }
+        [DataMember]
+        public string userid { get; set; }
+        [DataMember]
+        public string timeIn { get; set; }
+        [DataMember]
+        public string timeOut { get; set; }
+        
+        [DataMember]
+        public string datecreated { get; set; }
+
+    }
+}
