c6540ad83d5dcd9cb4cac106dca779defd3fcf33
Jefferson Jadoc <70456980+ashwallbera@users.noreply.github.com>
2022-06-20 21:42:39 +0800

    [ADD] Controller for antendance

---
+++ JEAspNetCore/Controllers/AttendanceController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace JEAspNetCore.Controllers
+{
+    [Route("api/attendances")]
+    [ApiController]
+    public class AttendanceController : ControllerBase
+    {
+        // GET: api/<AttendanceController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<AttendanceController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<AttendanceController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<AttendanceController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        [HttpPut("{id}/{timeOut}")]
+        public void timeOut(int id, [FromBody] string value)
+        {
+
+        }
+
+        // DELETE api/<AttendanceController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
