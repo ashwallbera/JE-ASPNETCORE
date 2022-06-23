a00c73fe680a0a141af38871ade9215f3723c81e
Jefferson Jadoc <70456980+ashwallbera@users.noreply.github.com>
2022-06-23 15:04:20 +0800

    [MOD] Add Projectid property

--- JEAspNetCore/Model/AttendanceModel.cs
+++ JEAspNetCore/Model/AttendanceModel.cs
@@ -8,14 +8,18 @@ namespace JEAspNetCore.Model
         [DataMember]
         public string id { get; set; }
         [DataMember]
+        public string projectid { get; set; }
+        [DataMember]
         public string userid { get; set; }
         [DataMember]
         public string timeIn { get; set; }
         [DataMember]
         public string timeOut { get; set; }
-        
+       
         [DataMember]
         public string datecreated { get; set; }
+
+       
 
     }
 }
