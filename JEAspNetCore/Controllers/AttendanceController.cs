2f2b123cd2c30a12a0ff6b706179827ab954b901
Jefferson Jadoc <70456980+ashwallbera@users.noreply.github.com>
2022-06-30 16:00:52 +0800

    [MOD] HTTP PUT for timeout

--- JEAspNetCore/Controllers/AttendanceController.cs
+++ JEAspNetCore/Controllers/AttendanceController.cs
@@ -60,6 +60,7 @@ namespace JEAspNetCore.Controllers
         {
             try
             {
+               
                 model.datecreated = DateTime.UtcNow.ToShortDateString();
                 //Check if the userid and date are existing to database && project
                 //Return Ok response
@@ -83,8 +84,10 @@ namespace JEAspNetCore.Controllers
                     Console.WriteLine(((AttendanceModel)attendanceModel.Object));*/
 
                 }
-               
-                model.timeIn = DateTime.UtcNow.ToShortTimeString();
+
+               model.id = Guid.NewGuid().ToString();
+
+               model.timeIn = DateTime.UtcNow.ToShortTimeString();
                 var result = await firebaseClient
                     .Child("attendance").
                     PostAsync(model);
@@ -96,15 +99,59 @@ namespace JEAspNetCore.Controllers
            
         }
 
-        // PUT api/<AttendanceController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        [HttpPut]
+        public async Task<ActionResult<IEnumerable<AttendanceModel>>> TimeOut(AttendanceModel model)
         {
-        }
+            try
+            {
+               
+                //Check if the userid and date are existing to database && project
+                //Return Ok response
+                //else if the user and date are not existing to database
+                var getAttendance = (await firebaseClient
+                   .Child("attendance")
+                  .OnceAsync<AttendanceModel>()).Where(attendance => 
+                  attendance.Object.id.Equals(model.id)
+                  && attendance.Object.datecreated.Equals(model.datecreated)
+                  && attendance.Object.projectid.Equals(model.projectid)
+                  ).FirstOrDefault();
 
-        [HttpPut("{id}/{timeOut}")]
-        public void timeOut(int id, [FromBody] string value)
-        {
+
+                if (getAttendance != null)
+                {
+                    getAttendance.Object.timeOut = DateTime.UtcNow.ToShortTimeString();
+                    await firebaseClient
+                        .Child("attendance")
+                        .Child(getAttendance.Key)
+                        .PutAsync(getAttendance.Object);
+                }
+              /*  List<AttendanceModel> attendanceModels = new List<AttendanceModel>();
+                foreach (var attendanceModel in getAttendance.ToList())
+                {
+                    AttendanceModel data = (AttendanceModel)attendanceModel.Object;
+                    if (data.id.Equals(model.id)
+                        && data.projectid.Equals(model.projectid)
+                        && data.userid.Equals(model.userid)
+                        && data.datecreated.Equals(model.datecreated))
+                    {
+                        return BadRequest("Alrady time in");
+                    }
+                    *//*  attendanceModels.Add((AttendanceModel)attendanceModel.Object);
+                      Console.WriteLine(((AttendanceModel)attendanceModel.Object));*//*
+
+                }*/
+                /*var result = await firebaseClient
+                    .Child("attendance").
+                    PostAsync(model);*/
+               
+                   
+
+                return Ok(getAttendance);
+            }
+            catch (Exception ex)
+            {
+                return Ok(ex.StackTrace);
+            }
 
         }
 
